ac66eb5dd2ab61fa225af42c25caf0c664550c3b
Carlos Osmar <carlososmarof@gmail.com>
2023-08-13 09:17:39 -0400

    com

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/Operaciones.java
@@ -0,0 +1,32 @@
+package com.example.myapplication2;
+
+public class Operaciones {
+    private int a;
+    private int b;
+
+    public Operaciones(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int sumar(){
+        return this.a + this.b;
+    }
+
+}
