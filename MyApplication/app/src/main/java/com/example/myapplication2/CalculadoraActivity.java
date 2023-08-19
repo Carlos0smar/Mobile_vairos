613e4cf1f0bbbb7e789ae1ef5da97c23e9d2e86c
Carlos Osmar <carlososmarof@gmail.com>
2023-08-19 15:12:29 -0400

    calculator_interface_1

---
+++ MyApplication/app/src/main/java/com/example/myapplication2/CalculadoraActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CalculadoraActivity extends AppCompatActivity {
+    Button button
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+
+
+}
