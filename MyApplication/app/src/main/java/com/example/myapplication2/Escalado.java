a60f59b4ca6a9213c636f0142745bfd0a89606ea
Carlos Osmar <carlososmarof@gmail.com>
2023-08-30 20:52:56 -0400

    escalado_2

--- MyApplication/app/src/main/java/com/example/myapplication2/Escalado.java
+++ MyApplication/app/src/main/java/com/example/myapplication2/Escalado.java
@@ -17,85 +17,34 @@ public class Escalado extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         canvas.drawColor(Color.BLACK);
         Paint paint = new Paint();
-
-
-
-        int RectanguloAlto= 0;
-        int RectanguloAncho = 0;
-
         paint.setColor(Color.WHITE);
+
         int ancho = getWidth();
         int alto = getHeight();
-        int TrianguloAlto= 1000;
-        int TrianguloAncho = 7000;
+        int FigAlto = 7000;
+        int FigAncho = 10000;
 
+        float scaleWidth = (float) ancho / FigAncho;
+        float scaleHeight = (float) alto / FigAlto;
+        float scaleFactor = Math.min(scaleWidth, scaleHeight);
 
-//
-//        if(TrianguloAlto >(alto)){
-//            TrianguloAlto =(TrianguloAlto/alto)+300;
-//        }
-//
-//        if(TrianguloAncho >(ancho)){
-//            TrianguloAncho =(TrianguloAncho/ancho)+300;
-//        }
+        if(FigAlto > alto/2 || FigAncho> ancho/2){
 
-        if(TrianguloAlto >(alto-300) || TrianguloAncho >(alto-300)){
-            if(TrianguloAlto>TrianguloAncho){
-            TrianguloAlto =(TrianguloAlto/(alto));
-            TrianguloAncho =(TrianguloAncho/(alto));
+            FigAlto = (int) ((FigAlto * scaleFactor)*0.5);
+            FigAncho = (int) ((FigAncho * scaleFactor)*0.5);
+        }
 
-            }
-            if(TrianguloAlto>TrianguloAncho) {
-                TrianguloAlto = (TrianguloAlto / (ancho));
-                TrianguloAncho = (TrianguloAncho / (ancho));
-            }
+        //Tríángulo
+        canvas.drawLine((float) (ancho * 0.05), (float) (alto * 0.05), (float) (ancho * 0.05), (float) (alto * 0.05 + FigAlto), paint);
+        canvas.drawLine((float) (ancho * 0.05), (float) (alto * 0.05 + FigAlto), (float) (ancho * 0.05 + FigAncho), (float) (alto * 0.05 + FigAlto), paint);
+        canvas.drawLine((float) (ancho * 0.05), (float) (alto * 0.05), (float) (ancho * 0.05 + FigAncho), (float) (alto * 0.05 + FigAlto), paint);
 
-            }
-
-//        if(TrianguloAncho >(ancho-300)){
-//        }
-//        PARA VER EL ALTO 2281 Y ANCHO  1080 DE LA PANTALLA
-        Log.d(TAG, "Ancho = " + ancho + "Alto = " + alto);
-//        canvas.drawLine(10,10,100,100, paint);
-
-        Double Hipotenusa = Math.sqrt(Math.pow(TrianguloAncho, 2) + Math.pow(TrianguloAlto,2));
-
-
-        canvas.drawLine((float)(ancho*0.08) , (float)(alto*0.08), (float)(ancho*0.08), (float)(alto*0.08+TrianguloAlto), paint);
-        canvas.drawLine((float)(ancho*0.08 ) , (float)(alto*0.08+ TrianguloAlto), (float)(ancho*0.08 + TrianguloAncho), (float)(alto*0.08+TrianguloAlto), paint);
-        canvas.drawLine((float)(ancho*0.08 ) , (float)(alto*0.08), (float)(ancho*0.08 + TrianguloAncho), (float)(alto*0.08+ TrianguloAlto), paint);
-
-        canvas.drawLine((float)(ancho*0.08) , (float)(alto*0.6), (float)(ancho*0.08), (float)((alto*0.6)+TrianguloAlto), paint);
-        canvas.drawLine((float)(ancho*0.08) , (float)(alto*0.6), (float)((ancho*0.08)+TrianguloAncho), (float)(alto*0.6), paint);
-        canvas.drawLine((float)(ancho*0.08) , (float)(alto*0.6 + TrianguloAlto), (float)((ancho*0.08)+TrianguloAncho), (float)(alto*0.6+ TrianguloAlto), paint);
-        canvas.drawLine((float)(ancho*0.08 +TrianguloAncho) , (float)(alto*0.6 ), (float)((ancho*0.08)+TrianguloAncho), (float)(alto*0.6+ TrianguloAlto), paint);
-
-//
-//        canvas.drawLine(0, alto / 2, ancho, alto / 2, paint);
-//        canvas.drawLine(ancho / 2, 0, ancho / 2, alto, paint);
-
-//        float limInfx = -20;
-//        float limSupx = 20;
-//        float limInfy = -20;
-//        float limSupy = 20;
-
-
-//        for (float x = limInfx; x <= limSupx; x += 0.01) {
-//            double y = fx(x);
-////            Log.d(TAG, "x = "+x+ " y = " + y);
-//            double xt = ((x - limInfx) / (limSupx - limInfx)) * ancho;
-//            Log.d(TAG, "x = " + x + " xt = " + xt);
-//
-//            double yt = alto - (((y - limInfy) / (limSupy - limInfy)) * alto);
-//            Log.d(TAG, "y = " + y + " yt = " + yt);
-//
-//            canvas.drawCircle((float) xt, (float) yt, 5, paint);
-//        }
+        //Rectángulo
+        canvas.drawLine((float)(ancho*0.05) , (float)(alto*0.6), (float)(ancho*0.05), (float)((alto*0.6)+FigAlto), paint);
+        canvas.drawLine((float)(ancho*0.05) , (float)(alto*0.6), (float)((ancho*0.05)+FigAncho), (float)(alto*0.6), paint);
+        canvas.drawLine((float)(ancho*0.05) , (float)(alto*0.6 + FigAlto), (float)((ancho*0.05)+FigAncho), (float)(alto*0.6+ FigAlto), paint);
+        canvas.drawLine((float)(ancho*0.05 +FigAncho) , (float)(alto*0.6 ), (float)((ancho*0.05)+FigAncho), (float)(alto*0.6+ FigAlto), paint);
     }
-//    double fx(float x){
-//        return x*Math.atan(x);
-//    }
 }
