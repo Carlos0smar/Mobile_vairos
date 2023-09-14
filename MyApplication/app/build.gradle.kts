fff65126b672b4a7ac237d9c0a106636d88e8ebb
Carlos Osmar <carlososmarof@gmail.com>
2023-09-13 22:24:39 -0400

    listo

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
 //        exclude ' META-INF/NOTICE'
 //        exclude ' META-INF/notice.txt'
 //    }
-//    packagingOptions.resources.excludes.add("/META-INF/{ASL2.0,LICENSE,license.txt,NOTICE,notice.txt}")
-//    packagingOptions.resources.excludes.add('/META-INF/{AL2.0,LGPL2.1}')
+    packagingOptions.resources.excludes.add("/META-INF/{ASL2.0,LICENSE,license.txt,NOTICE,notice.txt}")
+    
 
 
 
@@ -45,13 +45,15 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("org.springframework.android:spring-android-rest-template:1.0.1.RELEASE")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
